cf010e89f82ad105eddde263c87ca35f7cab4cea
Haryana Tsaqib <haryfun@MacBook-Pro.local>
2024-11-12 23:52:43 +0700

    feat: create home work javascript basic part 1

---
+++ soal1.js
@@ -0,0 +1,10 @@
+// buatkan nama variable dengan masing2 nama "bebas" 
+// dengan masing2 value tipe data: number, string, boolean, array, object, null, undefined
+// dan dikasih keterangan di masing2 variable
+
+
+
+
+
+
+
