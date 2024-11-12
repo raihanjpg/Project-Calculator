1dd9d76b8c5f02383eed0995712d4eb53b9a9427
raihanjpg <raihanswag24@gmail.com>
2024-11-13 00:34:44 +0700

    Menjawab Soal Pertama

--- soal1.js
+++ soal1.js
@@ -2,9 +2,20 @@
 // dengan masing2 value tipe data: number, string, boolean, array, object, null, undefined
 // dan dikasih keterangan di masing2 variable
 
+let hanNumber = 17; // tipe data number
+let hanString = "Han"; // tipe data string
+let hanBoolean = true; //tipe data boolean
+let hanArray = [1, 2, 3]; //tipe data array
+let hanObjct = {nama: "han"}; // tipe data object
+let hanNull = null; // tipe data null
+let hanUndefined = undefined; //tipe data undefined 
 
-
-
-
+console.log("tipe data hanNumber : ", typeof hanNumber); 
+console.log("tipe data hanString :", typeof hanString);
+console.log("tipe data HanBoolean : ", typeof hanBoolean);
+console.log("tipe data hanArray :",typeof hanArray);
+console.log("tipe data hanObjct : ", typeof hanObjct);
+console.log("tipe data hanNull :", typeof hanNull);
+console.log("tipe data hanUndefined : ", typeof hanUndefined);
 
 
