7c1537aebb65c300aec62168d7d2277d2d163f20
haryanapnx <hary.vai@gmail.com>
2024-11-14 15:17:54 +0700

    feat: create home work javascript basic operator aritmath

---
+++ exercises/operatorAndComparation.js
@@ -0,0 +1,14 @@
+// buatlah contoh operator aritmatik penjumlahan, pengurangan dll (+, -, *, /, %)
+// berikan masing2 contoh dari operator aritmatik tersebut
+// contoh: 
+// let a = 2
+// let b = 30
+// let output = a + b // penjumlahan
+// console.log(output) // output: 32
+
+// dan seterusnya sesuai operator aritmatik lainnya
+
+
+
+//==================
+
