f004d0fdf9f16e1fa4c8974eccc2640721453423
haryanapnx <hary.vai@gmail.com>
2024-11-15 21:18:28 +0700

    create new exercise

---
+++ exercises/function.js
@@ -0,0 +1,9 @@
+// buatlah sebuah function javascript yang me-return sebuah string
+// berikan satu parameter yaitu "nama"
+// contoh: (input: raihan) 
+// (output: halo nama saya raihan)
+
+
+// ===============
+
+
