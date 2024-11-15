3fcb3b27531d24aa920ffd24af068c2e9645a0d5
raihanjpg <raihanswag24@gmail.com>
2024-11-15 15:34:26 +0700

    working on math problems operators and compration

--- exercises/operatorAndComparation.js
+++ exercises/operatorAndComparation.js
@@ -8,7 +8,26 @@
 
 // dan seterusnya sesuai operator aritmatik lainnya
 
+let input1 = 2;
+let input2 = 10;
 
+let penjumlahan = input1 + input2;
+console.log("Penjumlahan :", penjumlahan ); // output: 12
+
+let pengurangan = input2 - input1;
+console.log("Pengurangan:", pengurangan); // output: 8
+
+let perkalian = input1 * input2;
+console.log("perkalian:", perkalian); // output: 20
+
+let pembagian = input2 / input1;
+console.log("pembagian:", pembagian); //output: 5
+
+let sisaBagi = input1 % input2;
+console.log("sisa bagi :", sisaBagi); // output 2
+
+let pemangkatan = input1 ** input2;
+console.log("pemangkatan:", pemangkatan) // output 1024
 
 //==================
 
