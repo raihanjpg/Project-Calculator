0b4fd68587cd4af4242bce059c7098d496ca5428
raihanjpg <raihanswag24@gmail.com>
2024-11-19 00:25:19 +0700

    working on questions javascript conditional, if and else

--- exercises/conditional.js
+++ exercises/conditional.js
@@ -5,6 +5,14 @@
 // jika tidak maka console.log("input 2 lebih besar dari input 1")
 
 
+let input1 = 1;
+let input2 = 2;
+
+if (input1 > input2 ) {
+    console.log("input 1 lebih besar dari input 2");
+} else {
+    console.log("input 2 lebih besar dari input 1");
+};
 // ==============
 
 // soal 2
@@ -12,6 +20,19 @@
 // lalu buatlah sebuah pengkondisian jika input1 lebih besar dari input2
 // maka console.log("input 1 lebih besar dari input 2")
 // jika tidak maka console.log("input 2 lebih besar dari input 1")
+
+
+function perbandingan (input1, input2){
+    if (input1 > input2){
+        console.log("input 1 lebih bsar dari input 2");
+    }else{
+        console.log("input 2 lebih besar dari input 1");
+    }
+}
+    
+perbandingan(17, 5);
+perbandingan(5, 17);
+
 
 
 // ===========
